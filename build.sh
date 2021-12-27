9383d4a6138e0a4bbe10407947b440b46684038f
biud436 <biud436@gmail.com>
2021-12-27 14:42:33 +0900

    fix

--- build.sh
+++ build.sh
@@ -3,10 +3,16 @@
 # adding force executing
 set -x
 cd ./file_copy
+
+# compile program
 make
 
 # ignore standard output.
 mkdir ../bin >/dev/null 2>&1
+
+# copy file
 cp ./app ../bin/app
+
+# remove created file after compiling
 rm -f ./app
 rm -f ./main.o
